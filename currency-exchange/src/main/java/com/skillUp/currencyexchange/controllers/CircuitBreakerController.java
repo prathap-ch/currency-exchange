36a9142c467ab0c28805eaf5365a47b16ec224d5
prathap <35505223+prathap-ch@users.noreply.github.com>
2021-08-04 22:34:48 +0530

    RateLimiter and circuit breaker

--- currency-exchange/src/main/java/com/skillUp/currencyexchange/controllers/CircuitBreakerController.java
+++ currency-exchange/src/main/java/com/skillUp/currencyexchange/controllers/CircuitBreakerController.java
@@ -7,13 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CircuitBreakerController {
 	private final Logger logger=LoggerFactory.getLogger(CircuitBreakerController.class);
 	@GetMapping("/sampleApi")
-	@Retry(name = "sample-api", fallbackMethod = "fallbackResponse")
+	//@Retry(name = "sample-api", fallbackMethod = "fallbackResponse") //OLD
+	@CircuitBreaker(name = "sample-api", fallbackMethod = "fallbackResponse")
+	@RateLimiter(name = "sample-api")
+	@Bulkhead(name = "sample-api")
 	public String sampleApi() {
 		logger.info("*************access sample API*************");
 		ResponseEntity<String> response= new RestTemplate().getForEntity("http://localhost:8080/sample", String.class);
